68466f57c9012890ed2d5a44e5a577772b0c3676
Ravi Adi Prakoso <92860523+raviadi12@users.noreply.github.com>
2024-05-04 21:40:04 +0700

    Pertemuan kita pertama :D

---
+++ script.js
@@ -0,0 +1,68 @@
+
+
+const kondisi = false; // Boolean
+const angka = 5; // Integer (Bulat)
+const angka_koma = 5.2 // Float (Bilangan Koma)
+const data_string = "Gavriel"; // String (Huruf)
+
+
+// Balok Fungsi
+
+function kali_2(angka) {
+    return angka/2
+}
+
+//console.log(kali_2(3)); // Output: 6
+
+
+// Gavriel
+function gavriel(a, b) {
+    return a/b
+}
+//console.log(gavriel(8, 2));
+
+
+
+
+
+// Buat fungsi arya, untuk mengkalikan hasil input a dikali b.
+
+// Contoh output: arya(2,3) outputnya = 6;
+// arya(4, 2) outputnya = 8
+function arya(a, b) {
+    return a * b;
+}
+
+// Contoh pemanggilan:
+var hasil = arya();
+//console.log("Hasil perkalian:", hasil); // Output: 66/
+
+
+// Ben
+function ben(a, b) {
+    return a - b;
+}
+//console.log(ben(6,3));
+// Buat fungsi Ben, untuk mengurangi input a dikurang b;
+// Contoh output: ben(2, 5) outputnya = -3
+
+
+
+// Matthew
+
+function matthew(a, b) {
+    return a + b;  // << --- hayooo tambah apalagi :D
+}
+
+//con
+
+// Buat fungsi Matthew, untuk menambahkan input a ditambah b;
+// Contoh output: ben(2, 5) outputnya = 7
+
+
+console.log(ben(2, 3)); // Harusnya outputnya minus 1
+console.log(matthew(3, 4)); // Harusnya outputnya 7
+console.log(arya(2, 4)); // Harusnya outputnya 8
+console.log(gavriel(2, 4)); // Harusnya outputnya 0.5
+
+
